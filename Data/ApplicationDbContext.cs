eb13d4d49cb0c0e577c22b90b54a86cd9f751d7a
Marat <447599489marat@gmail.com>
2024-03-19 19:32:00 +0300

    In ApplicationDbContext.cs was added db methods.Global usings added. ConnString(to DB) added. Dependencies added.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Reflection;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<UserModel> Users { get; set; }
+        
+        
+        /// <summary>
+        /// GET
+        /// </summary>/
+        public List<UserModel> GetAllUsers()
+        {
+            return Users.ToList();
+        }
+
+        public UserModel GetUserById(int id)
+        {
+            return Users.ToList().Find(user => user.id == id);
+        }
+        
+        public List<UserModel> GetUserByProperty(string propertyName, object propertyValue)
+        {
+            PropertyInfo property = typeof(UserModel).GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if (property == null)
+            {
+                throw new ArgumentException($"Property named '{propertyName}' is not found in User model.");
+            }
+
+            return Users.ToList().FindAll(user =>
+                property.GetValue(user)?.Equals(propertyValue) == true);
+        }
+
+        /// <summary>
+        /// OTHER
+        /// </summary>/
+
+        public void UpdateUserById(int id, UserModel newUser)
+        {
+             int newUserIndex=Users.ToList().IndexOf(this.GetUserById(id));
+             Users.ToList()[newUserIndex] = newUser;
+             this.SaveChangesAsync();
+
+        }
+
+
+        public void DeleteUserById(int id)
+        {
+            Users.Remove(this.GetUserById(id));
+            this.SaveChangesAsync();
+        }
+        
+    }
+}
+    
---
+++ Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        
+        [Required(ErrorMessage = "Enter text")]
+        [MaxLength(50, ErrorMessage = "Too long")]
+        public string name { get; set; }
+
+        [Range(0, 100, ErrorMessage = "unacceptable age")]
+        public int age { get; set; }
+
+        /*public User(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }    */
+
+        public override string ToString()
+        {
+            return $"{{\"Name\":\"{name}\",\"age\":{age}}}";
+        }
+        
+        
+
+    }
+}
