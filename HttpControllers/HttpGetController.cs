80ff2832d34c5c92f524390eb7a2731a68d0e551
Marat <447599489marat@gmail.com>
2024-04-08 20:26:41 +0300

    Paging to get methods added.

--- HttpControllers/HttpGetController.cs
+++ HttpControllers/HttpGetController.cs
@@ -27,6 +27,24 @@ public class HttpGetController:ControllerBase
         return NoContent();
     }
     
+    [HttpGet("get-all-paging-users/{pageNumber:int:min(1)}/{pageSize:int:min(1)}")]
+    [HttpGet("get-all-paging-users/{pageNumber:int:min(1)}&{pageSize:int:min(1)}")]
+    [HttpGet("get-all-paging-users")]
+    public async Task<IActionResult> GetAllPagingUsers(int? pageNumber, int? pageSize)
+    {
+        int CurrentPageNumber = pageNumber ?? 1;
+        int CurrentPageSize = pageSize ?? 5;
+        var users= await _context.GetAllUsers();
+
+        if (users.Count() != 0)
+        {
+            return Ok(users.Skip((CurrentPageNumber-1)*CurrentPageSize).Take(CurrentPageSize));
+            
+        }
+
+        return NoContent();
+    }
+    
     [HttpGet("get-user-by-id/{id:int:min(1)}")]
     public async Task<IActionResult> GetUserById(int id)
     {
@@ -43,7 +61,7 @@ public class HttpGetController:ControllerBase
 
     }
     
-    [HttpGet("get-users-by-property/{propertyName}/{propertyValue}")]
+    [HttpGet("get-users-by-property/{propertyName}/{propertyValue}/")]
     [HttpGet("get-users-by-property/{propertyName}&{propertyValue}")]
     public async Task<IActionResult> GetUsersByProperty(string propertyName, string propertyValue)
     {
@@ -51,6 +69,24 @@ public class HttpGetController:ControllerBase
         {
             var usersByProperty = await _context.GetUsersByProperty(propertyName, propertyValue);
             return Ok(usersByProperty);
+        }
+        catch(ArgumentException e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    
+    [HttpGet("get-paging-users-by-property/{propertyName}/{propertyValue}/{pageNumber:int:min(1)}/{pageSize:int:min(1)}")]
+    [HttpGet("get-paging-users-by-property/{propertyName}&{propertyValue}/{pageNumber:int:min(1)}&{pageSize:int:min(1)}")]
+    public async Task<IActionResult> GetPagingUsersByProperty(string propertyName, string propertyValue,int? pageNumber, int? pageSize)
+    {
+        int CurrentPageNumber = pageNumber ?? 1;
+        int CurrentPageSize = pageSize ?? 5;
+        try
+        {
+            var usersByProperty = await _context.GetUsersByProperty(propertyName, propertyValue);
+            return Ok(usersByProperty.Skip((CurrentPageNumber-1)*CurrentPageSize).Take(CurrentPageSize));
         }
         catch(ArgumentException e)
         {
